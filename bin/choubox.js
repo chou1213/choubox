faecb6c91a58a43480bda938711d7e9a7b03dff7
chou1213 <joan.zhou2@gmail.com>
2018-08-21 18:22:17 +0800

    init

---
+++ bin/choubox.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+const path = require('path');
+
+const package = require("../package.json");
+
+const program = require('commander');
+
+program
+    .version(package.version)
+    .usage('<command> [options]')
+    .description(package.description);
+
+program
+    .command('create <dir>')
+    .description('create a development directory')
+    .action((dir, cmd) => {
+        // console.log(dir, cmd);
+        require('../lib/create')(dir, cmd);
+    })
+
+program.parse(process.argv)
