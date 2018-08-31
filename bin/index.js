700fab04b431d67c842973a82d9556d494bdde90
joanChen <joan.zhou2@gmail.com>
2018-09-01 00:21:13 +0800

    整理文件夹

--- bin/index.js
+++
@@ -1,6 +0,0 @@
-const nunjucks = require('nunjucks');
-const path = require('path');
-nunjucks.configure(path.resolve(__dirname, '../src/demo'), { autoescape: true })
-var res = nunjucks.render('./config.js', { dir: 'James', date: `${new Date().getFullYear()}` });
-console.log(res);
-// fs.writeFileSync(path.resolve(path.resolve(__dirname, '../src/demo/config.js'), './config.js'), res)
